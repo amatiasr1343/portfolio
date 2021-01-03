3d0e9a1af7d2ed86e9dbae888cf28497bd3bd4ab
amatiasr1343 <76900498+amatiasr1343@users.noreply.github.com>
2021-01-03 07:17:14 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+function mostrarfoto () {
+
+	document.getElementById('foto').style.display = 'block';
+	document.getElementById('web').style.display = 'none';
+	document.getElementById('video').style.display = 'none';
+	document.getElementById('disenografico').style.display = 'none';
+
+	}
+
+
+
+
+function mostrarweb () {
+
+	document.getElementById('foto').style.display = 'none';
+	document.getElementById('web').style.display = 'block';
+	document.getElementById('video').style.display = 'none';
+	document.getElementById('disenografico').style.display = 'none';
+
+
+	}
+
+
+
+
+
+function mostrarvideo () {
+
+	document.getElementById('foto').style.display = 'none';
+	document.getElementById('web').style.display = 'none';
+	document.getElementById('video').style.display = 'block';
+	document.getElementById('disenografico').style.display = 'none';
+
+	}
+
+
+
+
+
+function mostrardisenografico () {
+
+	document.getElementById('foto').style.display = 'none';
+	document.getElementById('web').style.display = 'none';
+	document.getElementById('video').style.display = 'none';
+	document.getElementById('disenografico').style.display = 'block';
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
