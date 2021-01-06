10e6b8487abd65a3669ec1912f4cba776ab74f95
amatiasr1343 <76900498+amatiasr1343@users.noreply.github.com>
2021-01-06 17:53:57 -0300

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,47 +1,63 @@
 <?php
-   error_reporting(0);
-   
-   $Nombre = $_POST['Nombre'];
-   $Correo = $_POST['Correo'];
-   $Mensaje = $_POST['Mensaje'];
-   
-   if ($Nombre=='' || $Correo=='' || $Mensaje==''){
-   
-      echo "<script>alert('Los campos marcados con * son obligatorios');location.href ='javascript:history.back()';</script>";
-   
-   }else{
-   
-       date_default_timezone_set('America/Argentina/Buenos_Aires');
-       require("phpmail/phpmailer.php");
-       include("phpmail/smtp.php");
-       
-      $mail = new PHPMailer(true); // the true param means it will throw exceptions on errors, which we need to catch
-      
-      $mail->IsSMTP(); // telling the class to use SMTP
-      $cuerpo="Nombre: ".$Nombre."<br />Email: ".$Correo."<br />Mensaje: ".$Mensaje."<br />";
-      try {
-         $mail->Host       = "mail.zerpens.com"; // SMTP server
-         $mail->SMTPAuth   = true;                  // enable SMTP authentication
-         $mail->Port       = 25;                    // set the SMTP port for the GMAIL server
-         $mail->Username   = "webportfolio@gmail.com"; // SMTP account username
-         $mail->Password   = "qwerty1343";        // SMTP account password
-         $mail->AddReplyTo($Correo, $Nombre);
-         $mail->AddAddress('webportfolio@gmail.com', 'NOMBRE');
-         $mail->SetFrom("matiasar1343@gmail.com", $Nombre);
-         $mail->IsHTML(true);
-         $mail->Subject = 'ASUNTO DEL MAIL';
-         
-         $mail->MsgHTML($cuerpo);
-         $mail->AltBody = 'Nombre: '.$Nombre.' \n<br />'.
-         'Email: '.$Correo.' \n<br />'.
-         'Mensaje: '.$Mensaje.' \n<br />';
-         $mail->Send();
-         echo "<script>alert('Muchas gracias por contactarnos! Pronto recibirás una respuesta');location.href ='javascript:history.back()';</script>";
-      } catch (phpmailerException $e) {
-         echo $e->errorMessage(); //Pretty error messages from PHPMailer
-      } catch (Exception $e) {
-         echo $e->getMessage(); //Boring error messages from anything else!
-      }
-   }
-   
+
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+
+
+$body = "nombre: " . $nombre . "<br>correo: " . $correo . "<br>mensaje: " . $mensaje;
+
+
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmail/Exception.php';
+require 'phpmail/PHPMailer.php';
+require 'phpmail/SMTP.php';
+
+
+
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'miwebportfolio@gmail.com';                     // SMTP username
+    $mail->Password   = 'qwerty1343';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('miwebportfolio@gmail.com', 'MI WEB');
+    $mail->addAddress('matiasar1343@gmail.com');     // Add a recipient
+
+
+    // Attachments
+
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Mensaje Web';
+    $mail->Body    = $body;
+ 	$mail->Charset = 'UTF-8';
+
+    $mail->send();
+    echo '<script>
+    			alert("Mensaje enviado muchas gracias");
+    			windows.history.go(-1);
+    			</script>';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+
 ?>
