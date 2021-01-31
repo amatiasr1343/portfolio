12fe41ddc1fdd118465020ec5b860006b337af60
amatiasr1343 <76900498+amatiasr1343@users.noreply.github.com>
2021-01-31 15:16:50 -0300

    Add files via upload

---
+++ js/animacion.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+
+
+
+
+  sr.reveal('.animacion-inicio', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+
+
+
+sr.reveal('.animacion-cosas', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-100px'
+  });
+
+
+
+sr.reveal('.animacion-form', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+  });
